7a214ab2ea91cca950ecddd6b6197685d6a81e5d
Tiago <tiagoprysthon14@gmail.com>
2023-06-18 21:13:19 -0300

    [feat] - testing Ingredient class

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,20 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
 # Req 1
 def test_ingredient():
-    pass
+    # Act
+    shrimp = Ingredient("camarão")
+    ham = Ingredient("ham")
+
+    # Asserts
+    # TestIngredientInvalidName
+    assert shrimp.name == "camarão"
+    # TestIngredientInvalidHashDifferent e TestIngredientInvalidHashEqual
+    assert hash(shrimp) == hash("camarão")
+    # TestIngredientInvalidRepr
+    assert repr(shrimp) == "Ingredient('camarão')"
+    # TestIngredientInvalidRestrictionMap
+    assert ham.restrictions == set()
+    # TestIngredientInvalidEqualDifferent] e TestIngredientInvalidEqualEqual
+    assert shrimp == Ingredient("camarão")
