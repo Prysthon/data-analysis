b958f1a539e08b52fb92b4fc66e99605acd3074e
Tiago <tiagoprysthon14@gmail.com>
2023-06-18 21:22:54 -0300

    [fix] - lint

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,14 @@
-from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.dish import Dish
 
 
 # Req 2
 def test_dish():
-    pass
+    # Arrange
+    # Act
+    shrimp = Dish("camarão", 30.00)
+    ham = Dish("ham", 15.00)
+    # Assert
+    assert shrimp.name == "camarão"
+    assert hash(shrimp) == hash(shrimp)
+    assert shrimp == Dish("camarão", 30.00)
+    assert shrimp != ham
