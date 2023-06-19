fe2607da235031fc9d3291bcc04da8ee6046814a
Tiago <tiagoprysthon14@gmail.com>
2023-06-18 22:19:03 -0300

    [feat] - menu_data function

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,22 @@
-# Req 3
+from models.dish import Dish
+from models.ingredient import Ingredient
+import pandas as pd
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        database = pd.read_csv(source_path)
+        self.dishes = set()
+
+        for food in database.groupby(["dish"]):
+            plate, data = food
+            price = data.iloc[0]["price"]
+            dish = Dish(plate, price)
+
+            recipe = list(zip(data["ingredient"], data["recipe_amount"]))
+
+            for ingredients, amount in recipe:
+                ingredient = Ingredient(ingredients)
+                dish.add_ingredient_dependency(ingredient, amount)
+            self.dishes.add(dish)
+
